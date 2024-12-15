a98e4795832703451cdcde0af3cd1989ed00ac0d
Patitocam <gerocamardelli@gmail.com>
2024-12-15 15:26:53 -0300

    cambios
    s

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField, Range(0.1f, 1f)] float timeScale = 0.5f; // Factor de velocidad del temporizador (0.5 = más lento, 1 = normal)
+    float elapsedTime;
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTime += Time.deltaTime * timeScale; // Escala la velocidad del temporizador
+        int score = Mathf.FloorToInt(elapsedTime); // Redondea a entero
+        timerText.text = score.ToString("D6"); // Formatea con 6 dígitos (000001, 000002, ...)
+    }
+}
